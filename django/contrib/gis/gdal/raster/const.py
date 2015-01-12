ca0aeb0d643f54cacb1dd33369b2874b66f6fa78
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2015-01-12 15:27:08 +0000

    Changed const structure

--- django/contrib/gis/gdal/raster/const.py
+++ django/contrib/gis/gdal/raster/const.py
@@ -1,67 +1,37 @@
 """
-Constants for raster handling: Header structures and Pixel types.
+Constants for raster handling: WKB Header structure and Pixel types.
 """
 from ctypes import c_byte, c_uint16, c_int16, c_uint32, c_int32, c_float,\
     c_double
 
 """
-Structure of a PostGIS Raster Header
-
-http://postgis.net/docs/RT_ST_MakeEmptyRaster.html
+PostGIS Raster Header Structure
 http://postgis.net/docs/doxygen/2.2/d8/d5e/structrt__raster__serialized__t.html
 """
-
 HEADER_NAMES = [
-    'endianness',
-    'version',
-    'nr_of_bands',
-    'scalex',
-    'scaley',
-    'originx',
-    'originy',
-    'skewx',
-    'skewy',
-    'srid',
-    'sizex',
-    'sizey'
+    'endianness', 'version', 'nr_of_bands', 'scalex', 'scaley', 'originx',
+    'originy', 'skewx', 'skewy', 'srid', 'sizex', 'sizey'
 ]
 
 HEADER_STRUCTURE = 'B H H d d d d d d i H H'
 
 """
-GDAL
-
-Pixel data types
-
+GDAL Pixel data types
 http://www.gdal.org/gdal_8h.html#a22e22ce0a55036a96f652765793fb7a4
-
-GDT_Unknown  - Unknown or unspecified type 
-GDT_Byte     - Eight bit unsigned integer
-GDT_UInt16   - Sixteen bit unsigned integer
-GDT_Int16    - Sixteen bit signed integer
-GDT_UInt32   - Thirty two bit unsigned integer
-GDT_Int32    - Thirty two bit signed integer
-GDT_Float32  - Thirty two bit floating point
-GDT_Float64  - Sixty four bit floating point
-GDT_CInt16   - Complex Int16
-GDT_CInt32   - Complex Int32
-GDT_CFloat32 - Complex Float32
-GDT_CFloat64 - Complex Float64
 """
-
 GDAL_PIXEL_TYPES = {
-    0:  'GDT_Unknown',
-    1:  'GDT_Byte',
-    2:  'GDT_UInt16',
-    3:  'GDT_Int16',
-    4:  'GDT_UInt32',
-    5:  'GDT_Int32',
-    6:  'GDT_Float32',
-    7:  'GDT_Float64',
-    8:  'GDT_CInt16',
-    9:  'GDT_CInt32',
-    10: 'GDT_CFloat32',
-    11: 'GDT_CFloat64'
+    0:  'GDT_Unknown',  # Unknown or unspecified type 
+    1:  'GDT_Byte',     # Eight bit unsigned integer
+    2:  'GDT_UInt16',   # Sixteen bit unsigned integer
+    3:  'GDT_Int16',    # Sixteen bit signed integer
+    4:  'GDT_UInt32',   # Thirty two bit unsigned integer
+    5:  'GDT_Int32',    # Thirty two bit signed integer
+    6:  'GDT_Float32',  # Thirty two bit floating point
+    7:  'GDT_Float64',  # Sixty four bit floating point
+    8:  'GDT_CInt16',   # Complex Int16
+    9:  'GDT_CInt32',   # Complex Int32
+    10: 'GDT_CFloat32', # Complex Float32
+    11: 'GDT_CFloat64'  # Complex Float64
 }
 
 GDAL_PIXEL_TYPES_INV = {v: k for k, v in GDAL_PIXEL_TYPES.items()}
@@ -96,43 +66,22 @@ GDAL_RESAMPLE_ALGORITHMS = {
 GDAL_RESAMPLE_ALGORITHMS_INV = {v: k for k, v in GDAL_RESAMPLE_ALGORITHMS.items()}
 
 """
-POSTGIS
-
-Band Pixel Type definitions at line 186 of file librtcore.h
+POSTGIS Band Pixel Type 
+Definitions at line 186 of file librtcore.h
 http://postgis.net/docs/doxygen/2.2/d8/d4f/librtcore_8h_source.html#l00186
-
-Band header structure definition at line 2279 of file librtcore.h
-http://postgis.net/docs/doxygen/2.2/d8/d4f/librtcore_8h_source.html#l02279
-
-enum {
-    PT_1BB=0,     /* 1-bit boolean            */
-    PT_2BUI=1,    /* 2-bit unsigned integer   */
-    PT_4BUI=2,    /* 4-bit unsigned integer   */
-    PT_8BSI=3,    /* 8-bit signed integer     */
-    PT_8BUI=4,    /* 8-bit unsigned integer   */
-    PT_16BSI=5,   /* 16-bit signed integer    */
-    PT_16BUI=6,   /* 16-bit unsigned integer  */
-    PT_32BSI=7,   /* 32-bit signed integer    */
-    PT_32BUI=8,   /* 32-bit unsigned integer  */
-    PT_32BF=10,   /* 32-bit float             */
-    PT_64BF=11,   /* 64-bit float             */
-    PT_END=13
-} rt_pixtype;
-
 """
-
 POSTGIS_PIXEL_TYPES = {
-    0:  '1BB',
-    1:  '2BUI',
-    2:  '4BUI',
-    3:  '8BSI',
-    4:  '8BUI',
-    5:  '16BSI',
-    6:  '16BUI',
-    7:  '32BSI',
-    8:  '32BUI',
-    10: '32BF',
-    11: '64BF',
+    0:  '1BB',   # 1-bit boolean
+    1:  '2BUI',  # 2-bit unsigned integer
+    2:  '4BUI',  # 4-bit unsigned integer
+    3:  '8BSI',  # 8-bit signed integer
+    4:  '8BUI',  # 8-bit unsigned integer
+    5:  '16BSI', # 16-bit signed integer
+    6:  '16BUI', # 16-bit unsigned integer
+    7:  '32BSI', # 32-bit signed integer
+    8:  '32BUI', # 32-bit unsigned integer
+    10: '32BF',  # 32-bit float
+    11: '64BF',  # 64-bit float
     13: 'END'
 }
 
@@ -168,40 +117,21 @@ POSTGIS_TO_GDAL = {
 }
 
 """
-PYTHON
-
 Python Struct Types
-
 https://docs.python.org/2/library/struct.html#format-characters
-
-Format, C Type          Python Type Standard Size
-b - signed char         integer     1
-B - unsigned char       integer     1
-? - _Bool               bool        1
-h - short               integer     2
-H - unsigned short      integer     2
-i - int                 integer     4
-I - unsigned int        integer     4
-l - long                integer     4
-L - unsigned long       integer     4
-q - long long           integer     8
-Q - unsigned long long  integer     8
-f - float               float       4
-d - double              float       8
 """
-
 STRUCT_SIZE = {
-    'b': 1,
-    'B': 1,
-    '?': 1,
-    'h': 2,
-    'H': 2,
-    'i': 4,
-    'I': 4,
-    'l': 4,
-    'L': 4,
-    'f': 4,
-    'd': 8
+    'b': 1, # Signed char
+    'B': 1, # Unsigned char
+    '?': 1, # _Bool
+    'h': 2, # Short
+    'H': 2, # Unisgned short
+    'i': 4, # Interger
+    'I': 4, # Unsigned Integer
+    'l': 4, # Long
+    'L': 4, # Unsigned Long
+    'f': 4, # Float
+    'd': 8  # Double
 }
 
 GDAL_TO_STRUCT = {
